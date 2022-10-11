0c56d99db44ff3add27b433c7e7620ca841a8ab3
Javier <javierpr@gmail.com>
2022-10-11 12:22:42 -0500

    Mi primer incurción

---
+++ Prueba/src/ejemplos/ejemplo2.java
@@ -0,0 +1,43 @@
+package ejemplos;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class ejemplo2 {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ejemplo2 window = new ejemplo2();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ejemplo2() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
